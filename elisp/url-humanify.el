a02f077caf4a9fe98fb9d6ca2d873b61efc5dc8f
Tom Schutter <t.schutter@comcast.net>
2014-10-31 17:25:08 -0600

    url-humanify.el: fixed checkdoc warnings

--- elisp/url-humanify.el
+++ elisp/url-humanify.el
@@ -1,12 +1,17 @@
-;;; url-humanify.el
+;;; url-humanify.el --- break an URL up to make it readable
 
-;; From http://www.blogbyben.com/2013/09/emacs-function-humanifying-urls.html
+;;; Commentary:
+
+;; Modified from
+;; http://www.blogbyben.com/2013/09/emacs-function-humanifying-urls.html
+
+;;; Code:
 
 (defun url-humanify ()
   "Take the URL at point and make it human readable."
   (interactive)
   (let* ((area (bounds-of-thing-at-point 'url))
-         (num-params  (count-occurances-in-region "&" (car area) (cdr area)))
+         (num-params (count-matches "&" (car area) (cdr area)))
          (i 0))
     (beginning-of-thing 'url)
     (when (search-forward "?" (cdr area) t nil)
@@ -15,26 +20,18 @@
         (search-forward "&" nil t nil)
         (insert "\n  ")
         (save-excursion
-          (previous-line)
-          (beginning-of-line)
+          (forward-line -1)
           (let ((start (search-forward "="))
                 (end (search-forward "&")))
             (url-decode-region start end)))
-        (setq i (+ i 1))))))
+        (setq i (1+ i))))))
 
 (defun url-decode-region (start end)
-  "Replace a region with the same contents, only URL decoded."
+  "Replace a region from START to END with the same contents, only URL decoded."
   (interactive "r")
   (let ((text (url-unhex-string (buffer-substring start end))))
     (delete-region start end)
     (insert text)))
 
-(defun count-occurances-in-region (needle start end)
-  (save-excursion
-    (let ((found 0))
-      (goto-char start)
-      (while (search-forward needle end t nil)
-        (setq found (+ found 1)))
-      found)))
-
 (provide 'url-humanify)
+;;; url-humanify ends here
